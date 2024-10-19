d375ace8f9de4255a02c0be96d175624f7dc91ba
Dinesh <codinesh@live.com>
2024-10-20 00:48:53 +0530

    ready

--- babel.config.js
+++ babel.config.js
@@ -12,7 +12,7 @@ module.exports = function (api) {
         {
           root: ["./"],
           alias: {
-            "@": "./" // This matches your tsconfig paths
+            "@": "./" 
           },
         },
       ],
